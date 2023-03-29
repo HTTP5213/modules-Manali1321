61cbc993e5d4bf6ce07ecbd1cbc99c6d92e72544
Manali Patel <113155434+Manali1321@users.noreply.github.com>
2023-03-28 20:18:27 -0400

    updated

--- test/my-module.test.js
+++ test/my-module.test.js
@@ -4,14 +4,15 @@ import { theShire, statement } from '../public/my-module.js';
 
 import { expect } from 'chai';
 
-describe('The Shire', function() {
-    it('should be Hobbit Settlement', function() {
+describe('The Shire', function () {
+    it('should be Hobbit Settlement', function () {
         expect(theShire).to.be.an.instanceof(HobbitSettlement);
     });
 });
 
-describe('statement', function() {
-	it('Should return the correct string', () => {
-		expect(statement()).to.equal('The Shire was founded 739 years before Buckland.');
-	});
+describe('statement', function () {
+    it('Should return the correct string', () => {
+        expect(statement()).to.equal('The Shire was founded 739 years before Buckland.')
+
+    });
 });
